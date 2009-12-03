08a5387f8a6adadd9dac10f5a3816bf3f9cb61f3
David Li <taweili@yahoo.com>
2009-12-03 17:21:16 +0800

    get shop info for the current user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,10 @@
 class HomeController < ApplicationController
   acts_as_taobao_controller
+
+  def index
+    unless taobao_session.nil?
+      @user = taobao_session.invoke("taobao.user.get", 'nick' => taobao_session.top_params['visitor_nick'], 'fields' => 'nick,sex')
+      @shop = taobao_session.invoke("taobao.shop.get", 'nick' => taobao_session.top_params['visitor_nick'], 'fields' => 'sid, cid, title, nick, desc, bulletin, pic_path, created, modified')
+    end
+  end
 end
