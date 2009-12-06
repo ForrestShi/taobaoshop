464161de62344f14bc736ee948ade734a0a194a7
David Li <taweili@yahoo.com>
2009-12-06 12:07:43 +0800

    get products

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,8 @@ class HomeController < ApplicationController
     unless taobao_session.nil?
       @user = taobao_session.invoke("taobao.user.get", 'nick' => taobao_session.top_params['visitor_nick'], 'fields' => 'nick,sex')
       @shop = taobao_session.invoke("taobao.shop.get", 'nick' => taobao_session.top_params['visitor_nick'], 'fields' => 'sid, cid, title, nick, desc, bulletin, pic_path, created, modified')
+      @products = taobao_session.invoke("taobao.products.get", 'nick' => taobao_session.top_params['visitor_nick'], 'fields' => 'product_id, name, pic_path')
+      puts "products: #{@products.inspect}"
     end
   end
 end
